dba9c17c27f641af30e2ce50dcad06330f35614c
Alberto Quintana Criado <alberto.quintana.criado@proton.me>
2025-03-06 02:44:39 +0000

    Initial Commit

---
+++ StickyOverlayTests/StickyOverlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickyOverlayTests.swift
+//  StickyOverlayTests
+//
+//  Created by Alberto Quintana on 6/3/25.
+//
+
+import Testing
+@testable import StickyOverlay
+
+struct StickyOverlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
